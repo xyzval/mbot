03da87f179a6a372d00b1c10ec35aee647c36b33
xyzval <r8149762@gmail.com>
2025-06-28 20:38:43 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+API_ID = 123456
+API_HASH = 'abc123'
+BOT_TOKEN = '7921247390:AAHELKsIPDGyqDuybVsYvAeX-Sr0CKBXwfo'
+
+API_USER = 'nvtryn'
+API_PASS = 'Lugoblok@1'
+API_KEY = 'Fv2dN2rKpjSPDDyBaX'
+BASE_URL = 'https://api.hesdastore.com'
